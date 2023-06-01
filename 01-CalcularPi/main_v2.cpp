0db13613908b863fb49a252e0bdbd90740d751d3
EzPinto <ezpinto@frba.utn.edu.ar>
2023-06-01 09:06:28 -0300

    Version 2 del codigo, iteracion infinita

--- 01-CalcularPi/main_v2.cpp
+++ 01-CalcularPi/main_v2.cpp
@@ -11,10 +11,12 @@ double calcularPi() {
         signo *= -1;
         denominador += 2;
 
+        // Frenar la iteracion cuando la variable "pi" llegue al valor "Pi/4"
         if (static_cast<int>(pi * 1e7) == 7853981)
             break;
     }
 
+    // Multiplicar por 4 la variable "pi" para dar con el resultado pedido.
     return pi * 4;
 }
 
